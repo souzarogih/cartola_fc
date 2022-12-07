da5999d388f7f7eb2f713df33a89e38e84e2a288
HigorSouza <higor.andrade@hotmail.com>
2022-12-07 15:50:43 -0300

    Subindo o código fonte da aplicação

---
+++ app/admin.py
@@ -0,0 +1,18 @@
+from django.contrib import admin
+from .models import Player, Team, MyTeam, Match, Action
+
+
+class ActionInline(admin.TabularInline):
+    model = Action
+
+
+class MatchAdmin(admin.ModelAdmin):
+    inlines = [ActionInline]
+
+
+# Register your models here.
+admin.site.register(Player)
+admin.site.register(Team)
+admin.site.register(MyTeam)
+admin.site.register(Match, MatchAdmin)
+admin.site.register(Action)
